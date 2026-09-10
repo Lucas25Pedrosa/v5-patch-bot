5cad238bc7a0cb93f9b6bb9e3ca761aa95077c8d
Lucas25Pedrosa <41903995+Lucas25Pedrosa@users.noreply.github.com>
2026-09-10 00:25:21 -0300

    Fix FBOLED 0.5 Ptr build collision

---
+++ fboled-diagnostic/FBOLEDViewMapper05Build.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <math.h>
+
+// Build wrapper: avoid collision with the legacy MacTypes.h typedef named Ptr.
+#define Ptr FBOLEDPtr
+#include "FBOLEDViewMapper05.m"
